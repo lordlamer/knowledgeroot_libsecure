cad72c4a775e3beb489dffe637a2d74e19740516
lordlamer <lordlamer@lordlamer.de>
2012-12-14 21:01:24 +0100

    * check page title and content title

--- lib/LibsecureModule/Libsecure/Plugin.php
+++ lib/LibsecureModule/Libsecure/Plugin.php
@@ -13,20 +13,40 @@ class LibsecureModule_Libsecure_Plugin extends Zend_Controller_Plugin_Abstract {
 	$controller = $request->getControllerName();
 	$action = $request->getActionName();
 
+	// content check
 	if ($module == '' && $controller == 'content' && ($action == 'edit' || $action == 'new') && $this->getRequest()->getMethod() == 'POST') {
-	    $moduleConfig = Knowledgeroot_Registry::get('libsecure_config');
+	    // check content
+	    $this->checkParam($request, 'content');
+	    $this->checkParam($request, 'content-title');
+	}
 
-	    // htmlpurifier check
-	    if($moduleConfig->libsecure->htmlpurifier) {
-		$htmlpurifier = new LibsecureModule_Htmlpurifier();
-		$request->setParam('content', $htmlpurifier->check($request->getParam('content')));
-	    }
+	// page check
+	if ($module == '' && $controller == 'page' && ($action == 'edit' || $action == 'new') && $this->getRequest()->getMethod() == 'POST') {
+	    // check content
+	    $this->checkParam($request, 'content');
+	    $this->checkParam($request, 'page_title');
+	}
+    }
 
-	    // safehtml check
-	    if($moduleConfig->libsecure->safehtml) {
-		$safehtml = new LibsecureModule_Safehtml();
-		$request->setParam('content', $safehtml->check($request->getParam('content')));
-	    }
+    /**
+     * check specific parameter
+     *
+     * @param object $request
+     * @param string $name
+     */
+    public function checkParam(Zend_Controller_Request_Abstract $request, $name) {
+	$moduleConfig = Knowledgeroot_Registry::get('libsecure_config');
+
+	// htmlpurifier check
+	if($moduleConfig->libsecure->htmlpurifier) {
+	    $htmlpurifier = new LibsecureModule_Htmlpurifier();
+	    $request->setParam($name, $htmlpurifier->check($request->getParam($name)));
+	}
+
+	// safehtml check
+	if($moduleConfig->libsecure->safehtml) {
+	    $safehtml = new LibsecureModule_Safehtml();
+	    $request->setParam($name, $safehtml->check($request->getParam($name)));
 	}
     }
 
